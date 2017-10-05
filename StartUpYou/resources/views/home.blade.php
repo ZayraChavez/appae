3fe4f5a428eda4b6a651a16857454feeb5f7f6e9
angelgurrolac <angel_gurrolac@outlook.com>
2017-10-05 18:40:13 -0500

    Spanish Translation

--- StartUpYou/resources/views/home.blade.php
+++ StartUpYou/resources/views/home.blade.php
@@ -13,8 +13,7 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
-                    You are logged in!
+                    Ingresaste correctamente al sistema
                 </div>
             </div>
         </div>
--- StartUpYou/resources/views/welcome.blade.php
+++ StartUpYou/resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Start Up You</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -69,19 +69,13 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{ url('/home') }}">Inicio</a>
                     @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
+                        <a href="{{ url('/login') }}">Inicio de sesión</a>
+                        <a href="{{ url('/register') }}">Registro</a>
                     @endif
                 </div>
             @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Prueba
-                </div>
-            </div>
         </div>
     </body>
 </html>
